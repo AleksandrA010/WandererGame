0bb8641021333ec046fd419252f55e6626109140
Arkhipov Alexander <143943934+AleksandrA010@users.noreply.github.com>
2025-06-01 22:57:25 +0300

    Delete Source directory

--- Source/WandererGame/WandererGame.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WandererGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WandererGame, "WandererGame" );
